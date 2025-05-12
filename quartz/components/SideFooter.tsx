d9edaed6432c6c08e736bbe55f6d24d188c1dfcf
Armando <armandonavarro1024@gmail.com>
2025-05-12 09:54:56 -0500

    Quartz sync: May 12, 2025, 9:54 AM

--- quartz/components/SideFooter.tsx
+++ quartz/components/SideFooter.tsx
@@ -14,11 +14,11 @@ export default ((opts?: Options) => {
     const linksSide = opts?.linksSide ?? []
     return (
       <sidefooter class={classNames(displayClass, "side-footer")}>
-		<h4 style="color: #bebebe; font-size: 1.2rem;">Next Session: Tuesday, May 13 @7:00pm CT</h4>
+		<h4 style="color: #bebebe; font-size: 1.1rem;">Next Session: Tuesday, May 13 @7:00pm CT</h4>
 
-		<h4 style="text-align: left; width:49%; display: inline-block; color: #bebebe; font-size: 1.1rem;">Navigation</h4>
-		<h4 style="text-align: right; width:50%;  display: inline-block; color: #bebebe; font-size: 1.1rem;">
-			v0.57.12k
+		<h4 style="text-align: left; width:49%; display: inline-block; color: #00FF4A; font-size: 1.1rem;">Navigation</h4>
+		<h4 style="text-align: right; width:50%;  display: inline-block; color: #00FF4A; font-size: 1.1rem;">
+			v0.57.12l
 		</h4>
 		
           {Object.entries(linksSide).map(([text, linksSide]) => (
